c669960a47b1cc09357ca470ce27fcb4123e5681
Leonardo Rivera <leorivera_88@hotmail.com>
2026-05-26 11:08:57 -0400

    update text iMicroSeq (#515)

--- components/NoScopes.tsx
+++ components/NoScopes.tsx
@@ -45,7 +45,7 @@ const NoScopes = (): ReactElement => (
     `}
 	>
 		<p>
-			{'You are not authorized to submit data into the Canadian VirusSeq Data Portal. '}
+			{'You are not authorized to submit data into the Canadian iMicroSeq Data Portal. '}
 			{'In order to obtain the correct permissions, please contact '}
 			<EmailLink email="info@imicroseq-dataportal.ca">info@imicroseq-dataportal.ca</EmailLink>
 			{' with the following information. Permission should be granted within 2 business days.'}
@@ -60,7 +60,7 @@ const NoScopes = (): ReactElement => (
 			<li>The name of your institution.</li>
 			<li>The name of the lead investigator for your study.</li>
 			<li>The study ID (if known).</li>
-			<li>The email address you used to log in to the VirusSeq Data Portal.</li>
+			<li>The email address you used to log in to the iMicroSeq Data Portal.</li>
 		</ol>
 	</ErrorNotification>
 );
